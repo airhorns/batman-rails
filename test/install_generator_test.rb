47d71e7e8f050abc66be4da2d330ad279d58af25
Harry Brundage <harry.brundage@gmail.com>
2011-10-18 16:32:28 -0400

    Make install generator support application.js and application.js.coffee.

--- test/install_generator_test.rb
+++ test/install_generator_test.rb
@@ -2,64 +2,62 @@ require 'mocha'
 require 'test_helper'
 require 'generators/batman/install_generator'
 
-class InstallGeneratorTest < Rails::Generators::TestCase
-  tests Batman::Generators::InstallGenerator
-  
+module InstallGeneratorTests
+
   def setup
     mkdir_p "#{destination_root}/app/assets/javascripts"
-    cp fixture("application.js"), "#{destination_root}/app/assets/javascripts"
+    cp fixture(application_javascript_path), "#{destination_root}/app/assets/javascripts"
     Rails.application.class.stubs(:name).returns("Dummy::Application")
-    
     super
   end
-  
-  test "Assert Batman application file is created" do
+
+  def test_batman_application_file_is_created
     run_generator
-    
+
     assert_file "#{javascripts_path}/dummy.js.coffee" do |app|
       assert_match /window\.Dummy = class Dummy extends Batman\.App/, app
       assert_match /@run ->/, app
     end
   end
-  
-  test "Assert Batman application file is created for two word application name" do
+
+  def test_batman_application_file_is_created_for_two_word_application_name
     Rails.application.class.stubs(:name).returns("FooBar::Application")
     run_generator
-    
+
     assert_file "#{javascripts_path}/foo_bar.js.coffee" do |app|
       assert_match /window\.FooBar = class FooBar extends Batman\.App/, app
     end
   end
-  
-  test "Assert application require is properly setup for two word application name" do
+
+  def test_application_require_is_properly_setup_for_two_word_application_name
     Rails.application.class.stubs(:name).returns("FooBar::Application")
     run_generator
-    
-    assert_file "#{javascripts_path}/application.js", /require foo_bar/
+
+    assert_file "#{javascripts_path}/#{application_javascript_path}", /require foo_bar/
   end
-  
-  test "Assert Batman directory structure is created" do
+
+  def test_batman_directory_structure_is_created
     run_generator
-    
+
     %W{controllers models helpers}.each do |dir|
       assert_directory "#{javascripts_path}/#{dir}"
       assert_file "#{javascripts_path}/#{dir}/.gitkeep"
     end
   end
-  
-  test "Assert no gitkeep files are created when skipping git" do
+
+  def test_no_gitkeep_files_are_created_when_skipping_git
     run_generator [destination_root, "--skip-git"]
-    
+
     %W{controllers models helpers}.each do |dir|
       assert_directory "#{javascripts_path}/#{dir}"
       assert_no_file "#{javascripts_path}/#{dir}/.gitkeep"
     end
   end
-  
-  test "Assert application.js require batman, batman.jquery, batman.rails and dummy.js" do
+
+  def test_applicationjs_require_batman_jquery_rails_and_dummy
     run_generator
-    
-    assert_file "#{javascripts_path}/application.js" do |app|
+
+    assert_file "#{javascripts_path}/#{application_javascript_path}" do |app|
       %W{batman batman.jquery batman.rails}.each do |require|
         assert_match /require batman\/#{require}/, app
       end
@@ -79,4 +77,24 @@ class InstallGeneratorTest < Rails::Generators::TestCase
   def fixture(file)
     File.expand_path("fixtures/#{file}", File.dirname(__FILE__))
   end
+end
+
+class InstallGeneratorWithApplicationJavascriptTest < Rails::Generators::TestCase
+  tests Batman::Generators::InstallGenerator
+
+  def application_javascript_path
+    "application.js"
+  end
+
+  include InstallGeneratorTests
+end
+
+class InstallGeneratorWithApplicationCoffeescriptTest < Rails::Generators::TestCase
+  tests Batman::Generators::InstallGenerator
+
+  def application_javascript_path
+    "application.js.coffee"
+  end
+
+  include InstallGeneratorTests
 end
